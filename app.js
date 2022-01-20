d1a44329fa3a0664c09fffd21cd45b8bb4c1da24
Maye Hunt <1huntg03@solent.ac.uk>
2022-01-20 21:49:16 +0000

    troubleshooting

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "views")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
